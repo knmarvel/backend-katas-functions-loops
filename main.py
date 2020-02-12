63499183a907b4ed4f835da9c97bb1872bcf6160
Karen Marvel <knmarvel@gmail.com>
2020-02-12 10:56:20 -0500

    making the main.py functional

--- main.py
+++ main.py
@@ -1,39 +1,66 @@
 #!/usr/bin/env python
+import sys
+
+
 """Implements math functions without using operators except for '+' and '-' """
 
-__author__ = "???"
+
+__author__ = "github.com/knmavel"
 
 
 def add(x, y):
-    """Add two integers. Handles negative values."""
-    # your code here
-    return
+    print(x, y)
+    return x + y
 
 
 def multiply(x, y):
-    """Multiply x with y. Handles negative values of x or y."""
-    # your code here
-    return
+    print(x, y)
+    return x*y
 
 
 def power(x, n):
-    """Raise x to power n, where n >= 0"""
-    # your code here
-    return
+    print(x, n)
+    return x**n
 
 
 def factorial(x):
-    """Compute factorial of x, where x > 0"""
-    # your code here
-    return
+    print(x)
+    return math.factorial(x)
 
 
 def fibonacci(n):
-    """Compute the nth term of fibonacci sequence"""
-    # your code here
-    return
+    print (n)
+    return (str(n) + " ain't no easy check for fibbers")
+
+
+
+def main():
+    if len(sys.argv) < 3:
+        print ('usage: python main.py {--add | --multiply | --power | --factorial | fibonacci } {[x ,y] | [x, y] | [x, n] | [x] | [n]  ')
+        sys.exit(1)
+    if sys.argv[0] == "--add" or sys.argv[0] == "--multiply" or sys.argv[0] == "--power":
+        if len(sys.argv) != 5:
+            print("need two integer arguments for that function")
+    else:
+        if len(sys.argv) != 4:
+            print("need one integer parameter for that function")
+
+    option = sys.argv[1]
+    numbers = sys.argv[2:]
+    if option == '--add':
+        add(int(numbers[0]), int(numbers[1]))
+    elif option == '--multiply':
+        multiply(numbers)
+    elif option == '--power':
+        power(numbers)
+    elif option == '--factorial':
+        factorial(numbers)
+    elif option == '--fibonacci':
+        fibonacci(numbers)
+    else:
+        print ('unknown option: ' + option)
+        sys.exit(1)
 
 
 if __name__ == '__main__':
-    # your code to call functions above
-    pass
+    main()
